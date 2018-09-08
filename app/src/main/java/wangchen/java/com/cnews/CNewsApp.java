f3bf84b80e568fa6afabe1c60098c1781a857900
Clarence Wang <chwangthu@gmail.com>
2018-09-08 13:52:05 +0800

    add CNewsApp class

---
+++ app/src/main/java/wangchen/java/com/cnews/CNewsApp.java
@@ -0,0 +1,12 @@
+package wangchen.java.com.cnews;
+
+import android.app.Application;
+
+//To save global variables
+
+public class CNewsApp extends Application {
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+}
---
+++ app/src/main/java/wangchen/java/com/cnews/db/NewsDBHelper.java
@@ -0,0 +1,14 @@
+//package wangchen.java.com.cnews.db;
+//
+//import android.content.Context;
+//import android.database.sqlite.SQLiteOpenHelper;
+//
+//public class NewsDBHelper extends SQLiteOpenHelper {
+//
+//  public static final int DB_VERSION = 1;
+//  private String TABLE_NAME;
+//
+//  public NewsDBHelper(Context context) {
+//
+//  }
+//}
